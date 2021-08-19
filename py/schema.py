bd22d98f83633b9633e333a7df6bd6fc09b8c30e
papple23g <papple23g@gmail.com>
2021-08-19 10:20:09 +0800

    .

--- py/schema.py
+++ py/schema.py
@@ -5,10 +5,13 @@ import json
 from typing import List, Dict, Any, Union
 
 from py.utils import *
+Emoji = None
 
 
 @dataclass
 class EmojiQuery:
+    """ 查詢表符參數
+    """
     page: int = 1
     page_size_n: int = 30
     tags_str: str = None
@@ -22,21 +25,28 @@ class Tag:
     id: str = None
 
     async def popup_add_tags_input_box(self, ev):
+        """
+        1. 在標籤元素上顯示新增標籤的輸入框
+        2. 按下確定後送出請求並刷新表符的標籤列表區塊
+        """
+
+        # 獲取輸入框內的新增標籤列表字串 (ex. "tag1,tag2,tag3")
         tags_str: str = prompt('請輸入新增的標籤')
-        if tags_str:
-            clicked_span_elt = ev.currentTarget
-            emoji_id: str = clicked_span_elt.closest('tr').emoji.id
-            tag_spans_div = clicked_span_elt.closest('div.tag_spans')
+        if not tags_str:
+            return
 
-            emoji: Emoji = clicked_span_elt.closest('tr').emoji
+        # 獲取被點擊的標籤 SPAN 元素
+        clicked_span_elt = ev.currentTarget
 
-            # emoji.tag_list.append(Tag(name='tag_210818_165633', id='123'))
-            # tag_spans_div.clear()
-            # tag_spans_div <= emoji.tag_spans_div.children
+        # 自 TR 元素獲取表符物件
+        emoji: Emoji = clicked_span_elt.closest('tr').emoji
+        # 送出新增標籤請求後，獲取更新後的表符物件
+        emoji = await emoji.put_tags(tags_str)
 
-            emoji = await emoji.put_tags(tags_str)
-            tag_spans_div.clear()
-            tag_spans_div <= emoji.tag_spans_div.children
+        # 更新表符標籤區塊: 清空並重新添加
+        tag_spans_div = clicked_span_elt.closest('div.tag_spans')
+        tag_spans_div.clear()
+        tag_spans_div <= emoji.tag_spans_div.children
 
     @property
     def span(self) -> SPAN:
@@ -77,6 +87,21 @@ class Emoji:
     created_at: str
     average_hash_str: str
     tag_list: List[Tag]
+
+    @classmethod
+    def create(cls, emoji_dict: dict):
+        """ 輸入字典建立表符
+        """
+        return cls(**dict(
+            id=emoji_dict['id'],
+            url=emoji_dict['url'],
+            created_at=emoji_dict['created_at'],
+            average_hash_str=emoji_dict['average_hash_str'],
+            tag_list=[
+                Tag(**dict(id=tag_dict['id'], name=tag_dict['name']))
+                for tag_dict in emoji_dict['tag_list']
+            ],
+        ))
 
     @property
     def img_div(self) -> DIV:
@@ -162,33 +187,29 @@ class Emoji:
         tr.emoji = self
         return tr
 
-    def add_tag(self, tag: Tag):
-        self.tag_list.append(tag)
-
     @classmethod
     async def get_emoji_list(
             cls,
-            emojiQuery: EmojiQuery):
+            emojiQuery: EmojiQuery) -> List[Emoji]:
+        """ 根據搜尋條件取得表符列表
+
+        Args:
+            emojiQuery (EmojiQuery): 搜尋參數
+
+        Returns:
+            List[Emoji]
+        """
 
         emojiQuery_dict = {
             k: v for k, v in asdict(emojiQuery).items() if v is not None
         }
 
         return [
-            cls(**dict(
-                id=emoji_dict['id'],
-                url=emoji_dict['url'],
-                created_at=emoji_dict['created_at'],
-                average_hash_str=emoji_dict['average_hash_str'],
-                tag_list=[
-                    Tag(**dict(id=tag_dict['id'], name=tag_dict['name']))
-                    for tag_dict in emoji_dict['tag_list']
-                ],
-            ))
+            Emoji.create(emoji_dict)
             for emoji_dict in json.loads((await aio.get("/emoji", data=emojiQuery_dict)).data)
         ]
 
-    async def put_tags(self, tags_str: str):
+    async def put_tags(self, tags_str: str) -> Emoji:
         """ 更新表符標籤
         """
         emoji_dict = json.loads((await aio.ajax(
@@ -198,24 +219,25 @@ class Emoji:
             data=json.dumps(dict(
                 tags_str=tags_str)
             ))).data)
-        return Emoji(**dict(
-            id=emoji_dict['id'],
-            url=emoji_dict['url'],
-            created_at=emoji_dict['created_at'],
-            average_hash_str=emoji_dict['average_hash_str'],
-            tag_list=[
-                Tag(**dict(id=tag_dict['id'], name=tag_dict['name']))
-                for tag_dict in emoji_dict['tag_list']
-            ],
-        ))
+        return Emoji.create(emoji_dict)
 
 
 @dataclass
 class EmojiTable:
+    """ 表符列表表格
+
+    Attributes:
+        emoji_list (List[Emoji]): 表符列表
+    """
     emoji_list: List[Emoji]
 
     @property
-    def tableCol_th_tr_thead(cls) -> THEAD:
+    def _tableCol_th_tr_thead(cls) -> THEAD:
+        """ 表格標題元素: 表符圖片、表符操作工具、標籤列表
+
+        Returns:
+            THEAD
+        """
         return THEAD([
             TR([
                 TH(
@@ -235,11 +257,18 @@ class EmojiTable:
 
     @property
     def table_div(self) -> DIV:
+        """ 表格區域元素
+
+        Returns:
+            DIV
+        """
         return DIV(
             TABLE(
                 [
-                    self.tableCol_th_tr_thead,
+                    # 表格標頭
+                    self._tableCol_th_tr_thead,
                 ]+[
+                    # 表格標頭
                     emoji.tr for emoji in self.emoji_list
                 ],
                 Class="w3-table-all"
